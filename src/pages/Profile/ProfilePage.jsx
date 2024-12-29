2568c1f275529be4411ec5f61106cf35a269c53a
marat <m.kadzhaev@technolab.com.ru>
2024-12-30 00:48:12 +0700

    upd ProfilePage

--- src/pages/Profile/ProfilePage.jsx
+++ src/pages/Profile/ProfilePage.jsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {Box, Button, Link, Typography} from "@mui/material";
+import {Box, Button, Collapse, IconButton, Link, Typography} from "@mui/material";
 import {getCurrentProfile} from "../../service/ProfileService.js";
 import {getThemeColor} from "../../service/ThemeService.js";
 import {getSubscriptionPlanInvoiceLink} from "../../service/ShopService.js";
 import {useTelegram} from "../../hooks/useTelegram.js";
-
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 const ProfilePage = () => {
 
     const {tg, user} = useTelegram();
@@ -21,6 +22,12 @@ const ProfilePage = () => {
         })
     }
 
+    const [expandedSubscriptionPlan, setExpandedSubscriptionPlan] = useState(null);
+
+    const handleToggle = (subscriptionPlan) => {
+        setExpandedSubscriptionPlan(expandedSubscriptionPlan === subscriptionPlan ? null : subscriptionPlan);
+    };
+
     useEffect(() => {
         getCurrentProfile((data) => {
             setProfile(data);
@@ -29,6 +36,7 @@ const ProfilePage = () => {
 
     return (
         <div className="container">
+            <Typography display="flex" alignItems="center">balance:</Typography>
             <Box
                 sx={{
 
@@ -37,7 +45,7 @@ const ProfilePage = () => {
                     alignItems: "left",
                     position: "relative",
                     padding: "10px",
-                    marginBottom: "10px",
+                    marginBottom: "20px",
                     backgroundColor: "#1E1E1E"
                 }}>
                 {/* Баланс клиента */}
@@ -45,7 +53,6 @@ const ProfilePage = () => {
                     sx={{
 
                     }}>
-                    <Typography sx={{backgroundColor: "#3a3939"}}>balance:</Typography>
                     <Box display="flex" alignItems="center" mt={1}>
                         <Typography variant="h4" sx={{ width: 45, height: 45, mr: 1 }}>🎫</Typography>
                         <Typography variant="h5">{profile?.tickets || '0'}</Typography>
@@ -71,7 +78,7 @@ const ProfilePage = () => {
                     alignItems: "left",
                     position: "relative",
                     padding: "10px",
-                    marginBottom: "10px",
+                    marginBottom: "20px",
                     backgroundColor: "#1E1E1E"
                 }}>
 
@@ -91,6 +98,7 @@ const ProfilePage = () => {
 
 
             {/* Subscription plan */}
+            <Typography display="flex" alignItems="center">subscription plan: {profile?.subscriptionPlan || 'BASIC'}</Typography>
             <Box
                 sx={{
                     display: "flex",
@@ -105,28 +113,52 @@ const ProfilePage = () => {
                     overflow: "auto"
                 }}>
 
-                <Box
-                    sx={{
-                        marginBottom: "15px"
-                    }}>
-                    <Typography sx={{backgroundColor: "#3a3939"}}>subscription plan: {profile?.subscriptionPlan || 'BASIC'}</Typography>
+                <Box sx={{marginBottom: "10px"}}>
+                    <Button sx={{
+                        borderRadius: 0,
+                        width: "100%",
+                        padding: "10px",
+                        backgroundColor: "#3a3939"
+                    }} onClick={() => onBuyIntent('DEVELOPER')}>
+                        <Typography>Switch to DEVELOPER plan<br/>(15.000 ⭐ per month)</Typography>
+                        <IconButton onClick={() => handleToggle('DEVELOPER')}>
+                            {expandedSubscriptionPlan === 'DEVELOPER' ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                        </IconButton>
+                    </Button>
+
+                    <Collapse sx={{marginBottom: "10px", backgroundColor: "#3a3939"}} in={expandedSubscriptionPlan === 'DEVELOPER'} timeout="auto" unmountOnExit>
+                        <Box sx={{padding:"10px"}}>
+                            <Typography variant="body2">+ 3% chance of winning per lottery</Typography>
+                            <Typography variant="body2">+ Increased timecoin rewards</Typography>
+                            <Typography variant="body2">+ Increased f-tickets rewards</Typography>
+                        </Box>
+                    </Collapse>
                 </Box>
 
-                <Button sx={{
 
-                    padding: "10px",
-                    marginBottom: "10px",
-                    backgroundColor: "#3a3939"
-                }} onClick={() => onBuyIntent('DEVELOPER')}>
-                    <Typography>Switch to DEVELOPER plan<br/>(15.000 ⭐ per month)</Typography>
-                </Button>
-                <Button sx={{
+                <Box>
+                    <Button sx={{
+                        borderRadius: 0,
+                        width: "100%",
+                        padding: "10px",
+                        backgroundColor: "#3a3939"
+                    }} onClick={() => onBuyIntent('CO_FOUNDER')}>
+                        <Typography>Switch to CO-FOUNDER plan<br/>(50.000 ⭐ per month)</Typography>
+                        <IconButton onClick={() => handleToggle('CO_FOUNDER')}>
+                            {expandedSubscriptionPlan === 'DEVELOPER' ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                        </IconButton>
+                    </Button>
 
-                    padding: "10px",
-                    backgroundColor: "#3a3939"
-                }} onClick={() => onBuyIntent('CO_FOUNDER')}>
-                    <Typography>Switch to CO-FOUNDER plan<br/>(50.000 ⭐ per month)</Typography>
-                </Button>
+                    <Collapse sx={{backgroundColor: "#3a3939"}} in={expandedSubscriptionPlan === 'CO_FOUNDER'} timeout="auto" unmountOnExit>
+                        <Box sx={{padding:"10px"}}>
+                            <Typography variant="body2">+ DEVELOPER plan included</Typography>
+                            <Typography variant="body2">+ 7% chance of winning per lottery</Typography>
+                            <Typography variant="body2">+ Winning chances calculator</Typography>
+                            <Typography variant="body2">+ Total investors per lottery viewer</Typography>
+                        </Box>
+                    </Collapse>
+                </Box>
+
 
             </Box>
         </div>
